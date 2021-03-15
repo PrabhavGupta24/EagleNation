24a932732f26c7e54c296e7aba33864a5951509b
Jake Freeland <70768101+jakesfreeland@users.noreply.github.com>
2021-03-15 15:00:06 -0500

    Add files via upload

---
+++ EagleNation-macOS/Directories/BulletinView.swift
@@ -0,0 +1,20 @@
+//
+//  BulletinView.swift
+//  EagleNation-macOS
+//
+//  Created by 90308320 on 3/12/21.
+//
+
+import SwiftUI
+
+struct BulletinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BulletinView_Previews: PreviewProvider {
+    static var previews: some View {
+        BulletinView()
+    }
+}
